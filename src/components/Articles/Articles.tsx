17246ccce024074789218f5951b538dc4246d981
Егор Асонов <egorasonovvv@gmail.com>
2023-12-28 02:00:38 +0600

    [+] Second row of articles when pressing a button

--- src/components/Articles/Articles.tsx
+++ src/components/Articles/Articles.tsx
@@ -1,8 +1,10 @@
 import './Articles.styles.css'
 import Article from "./Article/Article.tsx";
 import {articles} from "../../models/Articles.ts";
+import {useState} from "react";
 
 function Articles(){
+    const [isExpanded, setIsExpanded] = useState(false);
     return(
         <div className="articlesBlock">
             <div className="articlesLabel">Check out our latest article</div>
@@ -14,7 +16,18 @@ function Articles(){
                 <Article image={articles[1].image} textBold={articles[1].textBold} textLight={articles[1].textLight}/>
                 <Article image={articles[2].image} textBold={articles[2].textBold} textLight={articles[2].textLight}/>
             </div>
-            <button className="button button-articles">View all</button>
+            {isExpanded && (
+                <div className="articles">
+                    <Article image={articles[0].image} textBold={articles[0].textBold} textLight={articles[0].textLight}/>
+                    <Article image={articles[1].image} textBold={articles[1].textBold} textLight={articles[1].textLight}/>
+                    <Article image={articles[2].image} textBold={articles[2].textBold} textLight={articles[2].textLight}/>
+                </div>
+            )}
+            <button
+                className="button button-articles"
+                onClick={() => setIsExpanded(!isExpanded)}>
+                {isExpanded ? "Hide" : "View all"}
+            </button>
         </div>
     )
 }
